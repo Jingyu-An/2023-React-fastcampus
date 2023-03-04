a081831ea6ebe83f92fcb5703e39027f5ff1ebb4
Jingyu-An <jqrohan@gmail.com>
2023-03-04 14:44:49 -0500

    React array CRUD

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,12 +1,87 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import './App.css';
-import InputSample from "./InputSample";
+import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email   : ''
+  });
+  
+  const {username, email} = inputs;
+  
+  const onChange = (e) => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  };
+  
+  const [users, setUsers] = useState([
+    {
+      id      : 1,
+      username: 'John',
+      email: 'john@example.com',
+      active: true
+    },
+    {
+      id      : 2,
+      username: 'Jane',
+      email   : 'jane@example.com',
+      active: false
+    },
+    {
+      id      : 3,
+      username: 'Tim',
+      email   : 'tim@example.com',
+      active: false
+    }
+  ]);
+  
+  const nextID = useRef(4);
+  
+  const onCreate = () => {
+    
+    const user = {
+      id: nextID.current,
+      username,
+      email
+    };
+    
+    setUsers([...users, user]);
+    // setUsers(users.concat(user))
+    
+    nextID.current += 1;
+    setInputs({
+      username: '',
+      email   : ''
+    })
+  };
+  
+  const onRemove = (id) => {
+    setUsers(users.filter(user => user.id !== id));
+  };
+  
+  const onToggle = (id) => {
+    setUsers(users.map(
+      user => user.id === id
+        ? { ...user, active: !user.active}
+        : user
+    ))
+  };
+  
   return (
-    <div>
-      <InputSample />
-    </div>
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onCreate={onCreate}
+        onChange={onChange}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    </>
   );
 }
 
---
+++ begin-react/src/UserList.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const User = ({user, onRemove, onToggle}) => {
+  const { username, email, id, active } = user;
+  return (
+    <div>
+      <b style={{
+        color: active ? 'green': 'black',
+        cursor: 'pointer'
+      }} onClick={() => onToggle(id)}>
+        {username}
+      </b>
+      <span>({email})</span>
+      <button onClick={() => onRemove(id)}>Delete</button>
+    </div>
+  );
+};
+
+const UserList = ({users, onRemove, onToggle}) => {
+  
+  return (
+    <div>
+      {
+        users.map(user =>
+          (<User
+            key={user.id}
+            user={user}
+            onRemove={onRemove}
+            onToggle={onToggle}
+          />))
+      }
+    </div>
+  );
+};
+
+export default UserList;
